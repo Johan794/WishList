fe735136d7bf8403bc9fc0d24a635a1ea2547f49
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-03-31 10:51:30 -0500

    Form handle change added

--- wishlist/src/pages/wishform.js
+++ wishlist/src/pages/wishform.js
@@ -1,25 +1,39 @@
-
-let stat = {
+import Header from '../components/Header'
+let state = {
     name: "",
     description: ""
 }
 
 let handleChage = e =>{
-    console.log(e)
+    
+    switch (e.target.name){
+        case "name": state.name = e.target.value; break;
+        case "description": state.description = e.target.value
+    } 
+    console.log("state name: "+ state.name)
+    console.log("state description: "+state.description )
+
+}
+
+let handleSubmit = e =>{
+    fetch ()
 }
 
 export default function wishform(req,res){
     return (
         <div>
-        <form>
-            <div>
-                <input type="text" placeholder="Name" onChange={handleChage}/>  
+            <Header title = "Wish Form"></Header>
+        <card className = "card w-50 m-auto mt-3">
+        <form className='form  '>
+            <div className='form-group p-2'>
+                <input className='form-control' type="text" placeholder="Name" onChange={handleChage} name = "name"/>  
                 
             </div>
-            <div>
-            <textarea placeholder="Description" ></textarea>
+            <div className='form-group p-2'>
+            <textarea className='form-control' placeholder="Description" onChange= {handleChage} name = "description"></textarea>
             </div>
         </form>
+        </card>
     </div>
     )
 }
