a58ea6672143274b593773a59fad40fc644b0cf2
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-03-31 20:38:39 -0500

    Wish form added

--- wishlist/src/pages/api/insert.js
+++ wishlist/src/pages/api/insert.js
@@ -1,14 +1,17 @@
 import db from '../../util/database'
 
-export default function handler(req,res){
+export default async function handler(req,res){
     const {method , body} = req;
         if (method === "POST"){
+            console.log(req)
             db.push({
                 name: body.name,
                 description: body.description
             })
 
-            res.send("ok");
+            res.send({
+                response: "hello world"
+            });
         }else{
             res.status(404)
         }
--- wishlist/src/pages/wishform.js
+++ wishlist/src/pages/wishform.js
@@ -10,27 +10,39 @@ let handleChage = e =>{
         case "name": state.name = e.target.value; break;
         case "description": state.description = e.target.value
     } 
-    console.log("state name: "+ state.name)
-    console.log("state description: "+state.description )
+    
 
 }
 
-let handleSubmit = e =>{
-    fetch ()
+let handleSubmit = async e =>{
+    e.preventDefault();
+    let config = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(state)
+      }
+    let r = await fetch ("http://localhost:3000/api/insert",config)
+    console.log(r);
 }
 
 export default function wishform(req,res){
     return (
         <div>
             <Header title = "Wish Form"></Header>
-        <card className = "card w-50 m-auto mt-3">
-        <form className='form  '>
+        <card className = "card w-50 m-auto mt-3 bg-primary">
+        <form className='form  ' onSubmit={handleSubmit}>
             <div className='form-group p-2'>
                 <input className='form-control' type="text" placeholder="Name" onChange={handleChage} name = "name"/>  
                 
             </div>
             <div className='form-group p-2'>
             <textarea className='form-control' placeholder="Description" onChange= {handleChage} name = "description"></textarea>
+            </div>
+            <div className='form-group p-2'>
+                <button type='submit'>let's go</button>
             </div>
         </form>
         </card>
